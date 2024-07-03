feb7b8f5b5cfa296b253cdbd7cd8aa45b6591fc3
Ian Tauro <sushruth127@gmail.com>
2024-07-03 19:36:38 +0530

    Window launching and clearing color

--- src/main/java/com/valproate/Main.java
+++ src/main/java/com/valproate/Main.java
@@ -1,17 +1,15 @@
 package com.valproate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.valproate.sandapp.SandApp;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Falling Sand Simulator");
+        config.setWindowedMode(1280, 720);
+        config.useVsync(true);
+        new Lwjgl3Application(new SandApp(), config);
     }
 }
---
+++ src/main/java/com/valproate/sandapp/SandApp.java
@@ -0,0 +1,16 @@
+package com.valproate.sandapp;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class SandApp extends ApplicationAdapter {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0, 1);
+    }
+}
